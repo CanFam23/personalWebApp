bba25c2c9005775935690b01946bb758c69e6f65
CanFam23 <nickclouse03@gmail.com>
2025-01-19 12:11:31 -0700

    Initial Commit

---
+++ src/test/java/com/clouse/personalApp/PersonalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clouse.personalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
