a0dc58456538abf75cc7c727e1ac557ae918a5ee
CanFam23 <nickclouse03@gmail.com>
2025-01-19 13:01:51 -0700

    Added controller for index page

---
+++ src/main/java/com/clouse/personalApp/web/controller/indexController.java
@@ -0,0 +1,13 @@
+package com.clouse.personalApp.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
